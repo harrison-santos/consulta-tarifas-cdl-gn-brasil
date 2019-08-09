0a5543acc4b39732a36fa129a742da1809412bec
harrison-santos <harrisonspeed1997@gmail.com>
2019-08-09 16:51:26 -0300

    Troca para o GeckoDriver Firefox

--- cdl_screenshots/Main.py
+++ cdl_screenshots/Main.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from datetime import date
 import sergas_screenshot
-
+import bahiagas_screenshot
+import algas_screenshot
 
 today = str(date.today().strftime("%d-%m-%Y"))
 
-options = webdriver.ChromeOptions()
+options = webdriver.FirefoxOptions()
+options.add_argument("--proxy-server='direct://'");
+options.add_argument("--proxy-bypass-list=*");
 options.headless = True
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Firefox(executable_path=r'C:\Program Files\GeckoDriver\geckodriver.exe')
 
-
-sergas_screenshot.captura(driver, today)
+#sergas_screenshot.captura(driver, today)
+#bahiagas_screenshot.captura(driver, today)
+algas_screenshot.captura(driver, today)
 driver.quit()
+
+
+
